3156cf777719b188cf72310669d92025799438f6
Behnia Farahbod <std_behnia.farahbod@khu.ac.ir>
2021-12-24 18:09:22 +0330

    Change instruction size to 32 bits.

--- instruction_memory.v
+++ instruction_memory.v
@@ -1,14 +1,14 @@
-module instrution_memory(clk, read_address, data_out);
+module instruction_memory(clk, read_address, instruction);
 
    input clk;
    input [63 : 0] read_address;
-   output [63 : 0] data_out; // طبق گفته ی استاد، تنها این مقدار باید 32 بیتی باشد. اما برای سادگی همان 64 بیتی( یعنی مانند بقیه) در نظرش می گیریم.
+   output [31 : 0] instruction; // دستور مورد نظر
 
-   reg [63 : 0] memory [1023 : 0]; // آرایه‌ی 1024 تایی از آدرس‌هایی که هر کدام 64 بیتی هستند.
+   reg [31 : 0] instructions [1023 : 0]; // آرایه‌ی 1024 تایی از دستورهایی که هر کدام 32 بیتی هستند.
 
    always @(posedge clk)
         begin
-            data_out = memory[read_address]
+            instruction = instructions[read_address]
         end
 
 endmodule;
