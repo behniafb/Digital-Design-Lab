b3d7668f6b56005fbc6252e768b5cde5524db9fb
Behnia Farahbod <std_behnia.farahbod@khu.ac.ir>
2021-12-24 17:49:31 +0330

    Update Data_Memory.v

--- Data_Memory.v
+++ Data_Memory.v
@@ -1,15 +1,14 @@
 module data_memory(clk, address, write_data, memRead, memWrite, read_data);
-	parameter n = 64;
 
 	input  clk;
-	input [n - 1 : 0] address;
-	input [n - 1 : 0] write_data;
+	input [63 : 0] address;
+	input [63 : 0] write_data;
 	input memRead;
 	input memWrite;
 
-	reg [n - 1 : 0] data[100-1: 0];
+	reg [63 : 0] data[100-1: 0];
 
-	output [n - 1 : 0] read_data;
+	output [63 : 0] read_data;
 
 	assign read_data = data[address];
 	
