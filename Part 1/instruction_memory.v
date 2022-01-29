9abc0cc858b20217fe7afcf437ebf4f06b7b78c4
Behnia Farahbod <std_behnia.farahbod@khu.ac.ir>
2022-01-29 14:36:19 +0330

    Update instruction_memory.v

--- Part 1/instruction_memory.v
+++ Part 1/instruction_memory.v
@@ -2,11 +2,12 @@ module instruction_memory(input clk, input [63 : 0] read_address, output [31 : 0
 
    reg [31 : 0] instructions [1023 : 0]; // آرایه‌ی 1024 تایی از دستورهایی که هر کدام 32 بیتی هستند.
    
-   // همانطور که در کلاس اشاره شد، برای نمونه چند دستور به طور دلخواه اضافه می کنیم.
-   assign instructions[0] = 32'b00000000000100001000000010110011;
-   assign instructions[1] = 32'b00000010010111110011000000100011;
-   assign instructions[2] = 8'h40538333; 
-   assign instructions[3] = 6'h4DB183;
+   // دستورات نمونه
+   assign instructions[0] = 32'b00000000000001010010101000000011;
+   assign instructions[1] = 32'b00000001010000000000101010110011;
+   assign instructions[2] = 32'b0100001010010101000001100110011; 
+   assign instructions[3] = 32'b00000001010001010010010000100011;
+   assign instructions[4] = 32'b11111111010110100000111001100111;
 
    always @(posedge clk)
         begin
